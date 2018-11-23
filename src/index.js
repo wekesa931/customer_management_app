f208e85bbbc035c9bdaefd7adeea4598154c7e8b
Bill Wekesa <wekesabill@gmail.com>
2018-11-23 10:59:56 +0300

    second commit

--- src/index.js
+++ src/index.js
@@ -1,17 +1,17 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import { BrowserRouter } from 'react-router-dom';
+import { HashRouter } from 'react-router-dom';
 import Routes from './routes';
 
 /* 
-React BrouwserRouter listens to events on change routes and initiates an
+React HashRouter listens to events on change routes and initiates an
 action thereafter e.g display the requested route
  */
 const App = () => {
     return(
-        <BrowserRouter>
+        <HashRouter>
             <Routes/>
-        </BrowserRouter>
+        </HashRouter>
     )
 }
 ReactDOM.render(<App />, document.getElementById('root'));
